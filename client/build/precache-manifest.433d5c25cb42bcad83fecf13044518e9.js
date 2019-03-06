58bb609a6c8d97c4e66e1aac404cb8878b27d1ba
thon maker <one23four5ate9@yahoo.com>
2019-03-06 16:09:53 -0500

    next

---
+++ client/build/precache-manifest.433d5c25cb42bcad83fecf13044518e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbba0149f6d0e7f19cac6b58ef70fb4a",
+    "url": "/static/media/tallplants.dbba0149.jpg"
+  },
+  {
+    "revision": "172934c6081f5c90d4b163118fe037e8",
+    "url": "/static/media/overHeadPlant.172934c6.png"
+  },
+  {
+    "revision": "9be3eeec5575aa2784705ef96c834b6a",
+    "url": "/static/media/dry.9be3eeec.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "595d6b009b67ff3a6367",
+    "url": "/static/js/main.595d6b00.chunk.js"
+  },
+  {
+    "revision": "e9956133a95bd1d67fd4",
+    "url": "/static/js/1.e9956133.chunk.js"
+  },
+  {
+    "revision": "595d6b009b67ff3a6367",
+    "url": "/static/css/main.5a48c4b5.chunk.css"
+  },
+  {
+    "revision": "271fe30477aea7f5ad534e6f75c61903",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const PORT =  process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+ if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"));
+ }
 
-// app.get("*", function(req,res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+ app.get("*", function(req,res) {
+   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+ });
 
 app.use(router);
 
